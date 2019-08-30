ce2f06183ae2185bef69ab56c27f756384df150e
Bobby <bobbyvanderaarde@gmail.com>
2019-08-30 12:55:26 -0700

    Cache inputs to remove input delay

--- ESS_Adapter_Basic.ino
+++ ESS_Adapter_Basic.ino
@@ -7,13 +7,21 @@
 #include <string.h>
 #include "Nintendo.h"
 
+/**
+ * Purple adapter uses 2-10
+ *                    (controller-console)
+ * Gold adapter uses 2-9? (need to test)
+ * Standard adapters use 6-3
+ * (First standard adapter uses 6-2)
+ */
+
 CGamecubeController GamecubeController(2);
 CGamecubeConsole GamecubeConsole(10);
 Gamecube_Data_t d = defaultGamecubeData;
 
 double default_range[] = {1, 71, 91, 105, 128, 151, 165, 185, 240};
 double x_range[] = {1, 71, 91, 125, 128, 131, 165, 185, 240};
-double y_range[] = {1, 71, 81, 110, 117, 131, 180, 200, 240};
+double y_range[] = {1, 71, 91, 110, 117, 124, 165, 200, 240};
 
 int x_cache[256]; 
 int y_cache[256];
@@ -25,36 +33,21 @@ void setup() {
 }
 void setup_cache(){
   for (int i = 0; i < 256; i++){
-    x_cache[i] = find_bands(i, x_range);
+    x_cache[i] = translate_input(i, true, x_range);
   }
   for (int i = 0; i < 256; i++){
-    y_cache[i] = find_bands(i, y_range);
+    y_cache[i] = translate_input(i, false, y_range);
   }
 }
 
-Gamecube_Report_t input_report;
-Gamecube_Report_t send_report;
-
-Gamecube_Report_t copy_report(Gamecube_Report_t input){
-  Gamecube_Report_t output;
-  memcpy(&output, &input, sizeof(input));
-  return output;
-}
-
+Gamecube_Report_t report;
 void take_input() {
   if (GamecubeController.read()){
-    input_report = GamecubeController.getReport();
-    make_output();
+    report = GamecubeController.getReport();
+    report.xAxis = x_cache[report.xAxis];
+    report.yAxis = y_cache[report.yAxis];
+    GamecubeConsole.write(report);
   }
-}
-
-void make_output(){
-  Gamecube_Report_t output_report = copy_report(input_report);
-  int xAxis = translate_input(output_report.xAxis, 1, x_range);
-  int yAxis = translate_input(output_report.yAxis, 0, y_range);
-  output_report.xAxis = xAxis;
-  output_report.yAxis = yAxis;
-  GamecubeConsole.write(output_report);
 }
 
 void loop() {
@@ -74,12 +67,12 @@ int find_bands(int x, double range[]){
 // input = controller input
 // type = 0 for x, 1 for y
 int translate_input(int input, int isX, double range[]){
-  int idx = 0;
-  if (isX){
-    idx = x_cache[input];
-  } else {
-    idx = y_cache[input];
-  }
+  int idx = find_bands(input, range);
+//  if (isX){
+//    idx = x_cache[input];
+//  } else {
+//    idx = y_cache[input];
+//  }
   if (idx == -1){
     Serial.println("Error finding band");
     return input; // Error finding the band, just return the default input
